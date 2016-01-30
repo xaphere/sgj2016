711346bec1e7e41c15526da37bf75a1917f81ca7
bspassov <bspassov@gmail.com>
2016-01-30 06:06:31 +0200

    added player prefab and collider prefab added player controller script

---
+++ Ritual/Assets/PlayerControl2D.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl2D : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    public float speed = 300.0f;
+    public float speedMultiplier = 1.0f;
+
+    public float confusedInterval = 0.0f;
+
+    public float pukeIntervalMin;
+    public float pukeIntervalMax;
+    public float pukeLength;
+
+    float confusedT = 0.0f;
+    float confusedLerp = 0.0f;
+    Vector2 confusedDir1;
+    Vector2 confusedDir2;
+
+    float drunkT = 0.0f;
+
+    float pukeT = 0.0f;
+    float pukeCycle = 0.0f;
+    bool isPuking = false;
+
+    public void MakeDrunk(float t)
+    {
+        drunkT = t;
+    }
+
+    public void MakeConfused(float t)
+    {
+        confusedT = t;
+        confusedDir1 = Random.insideUnitCircle;
+        confusedDir2 = Random.insideUnitCircle;
+    }
+
+    public void MakePuke(float t)
+    {
+        pukeT = t;
+    }
+
+    public void ResetModifiers()
+    {
+        drunkT = 0.0f;
+        confusedT = 0.0f;
+        pukeT = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float x = -Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        if (Input.GetButtonDown("Fire1")){ MakeConfused(2.0f); }
+        if (Input.GetButtonDown("Fire2")) { MakeDrunk(2.0f); }
+        if (Input.GetButtonDown("Fire3")) { MakePuke(2.0f); }
+        if (Input.GetButtonDown("Jump")) { ResetModifiers(); }
+
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        
+
+        if (confusedT > 0.0f)
+        {
+            confusedT -= Time.deltaTime;
+
+            if (confusedLerp <= 0.0f)
+            {
+                confusedDir1 = confusedDir2;
+                confusedDir2 = Random.insideUnitCircle;
+                confusedLerp = confusedInterval;
+            }
+            //xBias = Mathf.Lerp(confusedDir1.x, confusedDir2.x, confusedLerp);
+            //yBias = Mathf.Lerp(confusedDir1.y, confusedDir2.y, confusedLerp);
+            if (Mathf.Abs(x) > 0f || Mathf.Abs(y) > 0f)
+            {
+                confusedLerp -= Time.deltaTime;
+                //x += confusedDir1.x;
+                //y += confusedDir1.y;
+                x += Mathf.Lerp(confusedDir1.x, confusedDir2.x, confusedLerp);
+                y += Mathf.Lerp(confusedDir1.y, confusedDir2.y, confusedLerp);
+            }
+            Debug.DrawLine(transform.position, transform.position + new Vector3(confusedDir1.x, confusedDir1.y, 0f));
+        }
+
+        if (drunkT > 0.0f)
+        {
+            drunkT -= Time.deltaTime;
+            x *= -1;
+            y *= -1;
+        }
+
+        if (pukeT > 0.0f)
+        {
+            pukeCycle -= Time.deltaTime;
+            if (pukeCycle <= 0.0f)
+            {
+                isPuking = !isPuking;
+                pukeCycle = isPuking ? pukeLength : Random.Range(pukeIntervalMin, pukeIntervalMax);
+            }
+            if (isPuking)
+            {
+                x *= 0.0f;
+                y *= 0.0f;
+            }
+        }
+
+        Vector2 v = new Vector2(x, y).normalized * speed * Time.deltaTime;
+
+        rb.velocity = v;
+    }
+}
--- Ritual/Assets/Scripts/Mover.cs
+++ Ritual/Assets/Scripts/Mover.cs
@@ -9,21 +9,20 @@ public class Mover : MonoBehaviour {
 	}
 
     public float speed;
-
+    public float maxspeed;
     // Update is called once per frame
     void Update() {
         float x = -Input.GetAxis("Vertical");
         float z = Input.GetAxis("Horizontal");
         Vector3 dir = new Vector3(x, 0.0f, z).normalized;
         Vector3 moveVec = dir * speed;
-
-        GetComponent<Rigidbody>().AddForce(moveVec*100.0f);
-
-        /*
+        
         Rigidbody rb = GetComponent<Rigidbody>();
         RaycastHit hitInfo;
+
         if (rb.SweepTest(new Vector3(moveVec.x, 0.0f, moveVec.z), out hitInfo, moveVec.magnitude))
         {
+            /*
             if (rb.SweepTest(new Vector3(moveVec.x, 0.0f, 0.0f), out hitInfo, moveVec.magnitude))
             {
                 print("A");
@@ -37,10 +36,21 @@ public class Mover : MonoBehaviour {
                 moveVec.x *= 0.5f;
                 moveVec.z *= 0.0f;
             }
+            */
+            print("A");
+            print(-hitInfo.normal * Vector3.Dot(hitInfo.normal, new Vector3(moveVec.x, 0.0f, moveVec.z).normalized) * speed);
+            moveVec += -hitInfo.normal * Vector3.Dot(hitInfo.normal, new Vector3(moveVec.x, 0.0f, moveVec.z).normalized) * speed;
+            moveVec *= 0.5f;
         }
-        
+        if (rb.SweepTest(new Vector3(moveVec.x, 0.0f, moveVec.z), out hitInfo, Mathf.Min(0.1f, moveVec.magnitude * 2.0f)))
+        {
+            moveVec.x *= 0.0f;
+            moveVec.z *= 0.0f;
+        }
 
-        transform.position += moveVec;
-        */
+        if (moveVec.magnitude < 0.01)
+            moveVec *= 0.0f;
+        transform.Translate(moveVec);
+
     }
 }
