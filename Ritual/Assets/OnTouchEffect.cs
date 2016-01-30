f3f5503c9f2284809ec1d652a10ae1243452e7d6
bspassov <bspassov@gmail.com>
2016-01-30 12:30:55 +0200

    - forgotten commit

---
+++ Ritual/Assets/OnTouchEffect.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnTouchEffect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    public bool isKillEffect;
+    public bool isDrunkEffect;
+    public bool isConfusionEffect;
+    public bool isPukeEffect;
+    public bool isSpeedEffect;
+    public bool shouldResetEffects;
+    public float speedModifier;
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Ritual/Assets/PlayerTriggerScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTriggerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        ResolveTrigger(col, false);
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        ResolveTrigger(col, false);
+    }
+
+    void OnCollisionEnter2D(Collider2D col)
+    {
+        ResolveTrigger(col, true);
+    }
+
+    void OnCollisionStay2D(Collider2D col)
+    {
+        ResolveTrigger(col, true);
+    }
+
+    void ResolveTrigger(Collider2D col, bool isTrigger)
+    {
+        print("ResolveTrigger");
+        OnTouchEffect touchEffect = col.GetComponent<OnTouchEffect>();
+        if (!touchEffect)
+            return;
+
+        PlayerControl2D playerController = GetComponent<PlayerControl2D>();
+
+        if (touchEffect.shouldResetEffects)
+            playerController.ResetModifiers();
+        if (touchEffect.isConfusionEffect)
+            playerController.MakeConfused(180.0f);
+        if (touchEffect.isDrunkEffect)
+            playerController.MakeDrunk(180.0f);
+        if (touchEffect.isPukeEffect)
+            playerController.MakePuke(180.0f);
+        if (touchEffect.isSpeedEffect)
+            playerController.SetSpeedMultiplier(touchEffect.speedModifier);
+        if (touchEffect.isKillEffect)
+            GameObject.FindGameObjectWithTag("GM").GetComponent<GameplayManager>().ResetGame();
+    }
+}
