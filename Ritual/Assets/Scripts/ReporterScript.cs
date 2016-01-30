6531250b3539147b89721eb8054ed3b1561b60f0
xaphere <i.nikolov.90@gmail.com>
2016-01-30 04:28:32 +0200

    added base map object

---
+++ Ritual/Assets/Scripts/ReporterScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ReporterScript : MonoBehaviour {
+
+	private AudioSource audio;
+	private GameObject uiCanvas;
+
+	public GameObject textPrefab;
+	public string promptText;
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource> ();
+		uiCanvas = GameObject.FindWithTag ("UI/Canvas");
+		ReportText ();
+		Yell ();
+	}
+
+	void Yell() {
+		audio.Play ();
+	}
+
+	void ReportText() {
+		GameObject prompt = Instantiate (textPrefab);
+		prompt.transform.FindChild ("Text").GetComponent<Text> ().text = promptText;
+		prompt.transform.SetParent (uiCanvas.transform);
+		prompt.GetComponent<RectTransform> ().anchoredPosition = new Vector2 (-200.0f, 50.0f);
+		StartCoroutine (RemovePrompt (3, prompt));
+	}
+
+	IEnumerator RemovePrompt(int sec, GameObject prompt) {
+		yield return new WaitForSeconds (sec);
+		prompt.SetActive (false);
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag == "Player") {
+			ReportText ();
+			Yell ();
+		}
+	}
+	
+}
