d2e8cb2216e7b301c80ca3ffef63305b11357d4c
Stefan Doychev <sdoychev@gmail.com>
2016-01-30 05:43:22 +0200

    Update enemies behaviour

---
+++ Ritual/Assets/Scripts/KillPlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillPlayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag.Equals("Enemy"))
+        {
+            print("Game over!");
+            //Destroy(gameObject);
+        }
+    }
+
+}
--- Ritual/Assets/Scripts/PeriodicEnemy.cs
+++ Ritual/Assets/Scripts/PeriodicEnemy.cs
@@ -5,20 +5,48 @@ public class PeriodicEnemy : MonoBehaviour {
 
     public float repeatRate;
     private BoxCollider collider;
+    private Renderer renderer;
 
 	// Use this for initialization
 	void Start () {
         InvokeRepeating("ShowPeriodically", 0, repeatRate);
         collider = gameObject.GetComponent<BoxCollider>();
+        renderer = gameObject.GetComponent<Renderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (collider.enabled) {
+            renderer.material.color = Color.red;
+        }
+        else
+        {
+            renderer.material.color = Color.green;
+        }
 	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        killPlayer(col);
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        killPlayer(col);
+    }
+
+    void killPlayer(Collider col)
+    {
+        if (col.gameObject.tag.Equals("Player"))
+        {
+            print("Game over!");
+            //Destroy(gameObject);
+        }
+    }
 
     void ShowPeriodically()
     {
         collider.enabled = !collider.enabled;
     }
+
 }
