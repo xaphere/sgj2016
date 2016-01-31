aa21aa01722d8debfa94bb503cc327ddef68aeaa
xaphere <i.nikolov.90@gmail.com>
2016-01-31 13:56:43 +0200

    finished start scene

--- Ritual/Assets/MainMenuScript.cs
+++ Ritual/Assets/MainMenuScript.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class MainMenuScript : MonoBehaviour {
 
 	public void StartGame() {
+		AudioSource audio = GetComponent<AudioSource> ();
+		audio.Play ();
+		StartCoroutine (SwitchScenes());
+	}
 
+	IEnumerator SwitchScenes() {
+		yield return new WaitForSeconds (1);
+		SceneManager.LoadScene ("Scenes/Ivan");
 	}
 
 	public void Credits() {
-
+		gameObject.transform.FindChild ("CreditsPanel").gameObject.SetActive (true);
 	}
 
 	public void BackToMain() {
-
+		gameObject.transform.FindChild ("CreditsPanel").gameObject.SetActive (false);
 	}
 }
